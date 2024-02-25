558b28aa2c01bf57ebbe7dc1113f4596334afe6f
prodjohnper <JonathanPerez9743@gmail.com>
2024-02-25 15:00:52 -0400

    Update - Updated documentation

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,33 +1,47 @@
 #!/usr/bin/python3
 """
-    class FileStorage that serializes instances
-    to a JSON file and deserializes JSON file to instances
+    file_storage.py
+
+    Serializes instances to a JSON file and deserializes JSON file to instances
 """
 
 
 import json
 
 
-
 class FileStorage:
-
+    '''
+        Serializes and deserializes instances to and from JSON files
+    '''
     __file_path = "file.json"
     __objects = {}
 
-
-    def all(self): #Returns all stored objects.
+    def all(self):
+        '''
+            Returns all stored objects.
+        '''
         return FileStorage.__objects
 
-    def new(self, obj): #Adds a new object to the internal dictionary of objects.
+    def new(self, obj):
+        '''
+            Adds a new object to the internal dictionary of objects.
+        '''
         key = f"{obj.__class__.__name__}.{obj.id}"
         FileStorage.__objects[key] = obj
 
-    def save(self): #Saves the current state of objects to the JSON file.
+    def save(self):
+        '''
+            Saves the current state of objects to the JSON file.
+        '''
         with open(FileStorage.__file_path, "w") as file:
-            new_dict = {k: obj.to_dict() for k, obj in FileStorage.__objects.items()}
+            new_dict = {k: obj.to_dict()
+                        for k, obj in FileStorage.__objects.items()}
             json.dump(new_dict, file)
 
-    def reload(self): #Reloads objects from the JSON file.
+    def reload(self):
+        '''
+            Reloads objects from the JSON file
+        '''
         from models.base_model import BaseModel
         from models.user import User
         from models.amenity import Amenity
@@ -35,6 +49,7 @@ class FileStorage:
         from models.place import Place
         from models.review import Review
         from models.state import State
+
         try:
             with open(FileStorage.__file_path, "r") as file:
                 data = json.load(file)
